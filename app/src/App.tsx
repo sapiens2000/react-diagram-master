1c6d99dffc6deb13e20686d2817d41cd302b7165
ByeongGyu Jeon <cube9928@naver.com>
2023-05-18 16:58:59 +0900

    make new Project at First enter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,8 @@ import { ArrowLinkFactory } from './components/link/ArrowLinkFactory'
 import SelectNodeFactory from './components/node/SelectNodeFactory';
 import FilterNodeFactory from './components/node/FilterNodeFactory';
 import OutputNodeFactory from './components/node/OutputNodeFactory';
-import { SelectionBoxLayerFactory } from '@projectstorm/react-diagrams';
+import { ProjectDiagramModel } from './components/model/ProjectDiagramModel';
+import axios from 'axios';
 
 export default class App {
   protected activeModel: SRD.DiagramModel;
@@ -17,17 +18,16 @@ export default class App {
 	}
 
   public newModel(){
-    this.engine.setModel(this.activeModel);
+    //this.engine.setModel(this.activeModel);
     
     this.engine.getLinkFactories().registerFactory(new ArrowLinkFactory());
     this.engine.getNodeFactories().registerFactory(new OutputNodeFactory());
     this.engine.getNodeFactories().registerFactory(new FilterNodeFactory());
     this.engine.getNodeFactories().registerFactory(new SelectNodeFactory());
-    this.engine.getLayerFactories().registerFactory(new SelectionBoxLayerFactory());
 
-    console.log(this.engine.getLayerFactories())
-    const model = new SRD.DiagramModel()
-    
+
+    const model = this.makeNewProg();
+
     model.registerListener({
       linksUpdated: (event: any) => {
         event.link.registerListener({
@@ -37,9 +37,39 @@ export default class App {
         })
       }
     });
+ 
+    this.engine.setModel(model);  
+  }
 
-    this.engine.setModel(model);
-  
+  public makeNewProg(): ProjectDiagramModel{
+    var tmp_prog_mst = {
+        progId: -1,
+        progNm: "new project",
+        progDesc: "",
+        viewAttr: {},
+        useYn: false,
+        crtdDttm: "",
+        updtdttm: "",
+        dltDttm: ""
+      }
+ 
+    // 나중에 변경
+    axios.post("/diagram/project", tmp_prog_mst, { maxRedirects: 0})
+    .catch((Error) => { 
+      console.log(Error);
+    }).then(response => {
+      const regex = /프로젝트 ID: (\d+)/; 
+      const matches = response.data.match(regex);
+      if (matches) {
+        const id = parseInt(matches[1]);
+        tmp_prog_mst.progId = id;    
+      }
+    });
+
+    console.log(tmp_prog_mst);
+    const newModel = new ProjectDiagramModel();
+    newModel.setProgMst(tmp_prog_mst);
+    return newModel;
   }
 
 	public getActiveDiagram(): SRD.DiagramModel {
